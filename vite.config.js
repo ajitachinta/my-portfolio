3300eda9ffd2d2b8c7d57cb10d40cd06ac493391
Ajita Chintha <ajitachinta@gmail.com>
2026-06-17 12:17:16 -0700

    ci: deploy to GitHub Pages via Actions

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Repo is served from https://ajitachinta.github.io/my-portfolio/
+  base: '/my-portfolio/',
   plugins: [react()],
 })
